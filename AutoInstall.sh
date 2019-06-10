d4d170eeaed12cf877264d07deab852c2cbb5638
david <davidlo176@gmail.com>
2019-06-09 20:37:02 -0500

    agregadas opciones a AutoInstall.sh

--- AutoInstall.sh
+++ AutoInstall.sh
@@ -5,13 +5,17 @@ csv=programas.csv # nombre del csv con los programas por defecto
 
 function ayuda (){
     echo "Uso:
+        ./AutoInstall.sh -[comando] <argumento>
+                Nota: Si no se especifica un grupo se instalan
+                los programas de todos los grupos
+
+        -g: <grupo> Instala los programas en el grupo especificado.
         -h: Muestra esta pagina de ayuda
-        -d: Instalar todos los programas en el grupo por defecto
-        -a: Instala todos los programas
-        -g: Instala todos los programas en el grupo especificado
+        -p: <grupo> Instala los programas solo desde gestor de paquetes.
+        -s: <grupo> Instala los programas solo desde los scripts.
         "
 }
-function installGroup (){ # recibe el argumento: grupoAInstalar
+function getToInstall () {
     f='True'
     paquetes=()
     scripts=()
@@ -41,32 +45,45 @@ function installGroup (){ # recibe el argumento: grupoAInstalar
             f=False
         fi
     done
-    echo "Programas a instalar desde el gestor de paquetes: "${paquetes[@]}
+}
+function installGroup (){ # recibe el argumento: grupoAInstalar
+    getToInstall $1 
+    echo "Programas a instalar desde el gestor de paquetes: "
     echo "================================================"
-    echo " "
-    echo "Programas a instalar desde scripts: "${scripts[@]}
-    echo "=================================="
+    echo ${paquetes[@]}
     echo " "
     sudo apt install -y $(echo ${paquetes[@]})
-    installFromScript $scripts
-    
+    installFromScript $1
 }
 function installFromScript () {
+    getToInstall $1
+    echo "Programas a instalar desde scripts: "
+    echo "=================================="
+    echo ${scripts[@]}
+    echo " "
     for file in ${scripts[@]}
     do
         chmod +x scripts/$file
         scripts/$file
     done
 }
-#installDefaults
-
+function installFromManager () {
+    getToInstall $1
+    echo "Programas a instalar desde el gestor de paquetes:"
+    echo "================================================"
+    echo ${paquetes[@]}
+    sudo apt install -y $(echo ${paquetes[@]})
+}
 if [ "$1" == "-h" ] || [ "$1" == "" ]
 then
     ayuda
-elif [ "$1" == "-a" ]
-then
-    installGroup "" # instalar todas las aplicaciones
 elif [ "$1" == "-g" ]
 then
     installGroup "$2"
+elif [ "$1" == "-p" ]
+then
+    installFromManager "$2"
+elif [ "$1" == "-s" ]
+then
+    installFromScript "$2"
 fi
